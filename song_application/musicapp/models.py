d20f7c4d92976e316e73eb67ad8cf45012d0b690
temple7447 <Starukido@gmail.com>
2022-10-29 11:39:08 +0100

    django songs

--- song_application/musicapp/models.py
+++ song_application/musicapp/models.py
@@ -1,24 +1,25 @@
-from contextlib import nullcontext
-from operator import mod
-from statistics import mode
-from unittest.util import _MAX_LENGTH
+
 from django.db import models
 
 # Create your models here.
 
 class Artiste(models.Model):
-    first_name = models.CharField(_MAX_LENGTH = 200, null = True)
-    last_name = models.CharField(_MAX_LENGTH = 200)
-    age = models.IntegerField(_MAX_LENGTH =200)
+    first_name = models.CharField(max_length = 200, null = True)
+    last_name = models.CharField(max_length = 200)
+    age = models.IntegerField(max_length= 200)
     
     
 class Song(models.Model):
-    title = models.CharField(_MAX_LENGTH = 200, null = True)
+    title = models.CharField(max_length = 200, null = True)
     date = models.DateTimeField(auto_now_add = True)
-    likes = models.CharField(_MAX_LENGTH = 200)
-    artiste_id = models.CharField(_MAX_LENGTH = 200)
+    likes = models.CharField(max_length = 200)
+    artiste_id = models.CharField(max_length = 200)
     
     
 class Lyric(models.Model):
-    content = models.CharField(_MAX_LENGTH =500)
-    song_id = models.IntegerField(_MAX_LENGTH = 50)
+    content = models.CharField(max_length =500)
+    song_id = models.IntegerField(max_length = 50)
+
+
+def __str__(self):
+    return self.age
