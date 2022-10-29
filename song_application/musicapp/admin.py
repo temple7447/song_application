59bf872850762ba8b0b5ca3855433850235c067e
TEMPLE VOKE <62543994+temple7447@users.noreply.github.com>
2022-10-29 11:41:36 +0100

    Delete song_application directory

--- song_application/musicapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Artiste,Song,Lyric
-
-admin.site.register(Artiste)
-admin.site.register(Song)
-admin.site.register(Lyric)
